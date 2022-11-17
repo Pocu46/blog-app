7cc5502308aaa294cd69c8712d5b94a451c1a8b6
pocu46 <andrew.pulyaev@gmail.com>
2022-11-17 22:17:10 +0200

    validators are added

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,7 +1,8 @@
 import {Component} from "../core/component";
 import {Form} from "../core/form";
+import {Validators} from "../core/validators";
 
-export class CreateComponent extends Comment {
+export class CreateComponent extends Component {
   constructor(id) {
     super(id)
   }
@@ -10,8 +11,8 @@ export class CreateComponent extends Comment {
     this.$el.addEventListener('submit', submitHandler.bind(this))
 
     this.form = new Form(this.$el, {
-      title: [],
-      fulltext: []
+      title: [Validators.required],
+      fulltext: [Validators.required, Validators.minLenght(10)]
     })
   }
 }
@@ -19,10 +20,14 @@ export class CreateComponent extends Comment {
 function submitHandler(event) {
   event.preventDefault()
 
-  const formData = {
-    type: this.$el.type.value,
-    ...this.form.value()
-  }
+  if(this.form.isValid()) {
+    const formData = {
+      type: this.$el.type.value,
+      ...this.form.value()
+    }
 
-  console.log('Submit', formData)
+    console.log('Submit', formData)
+  } else {
+    console.warn('Form is invalid')
+  }
 }
